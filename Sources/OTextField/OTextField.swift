0a72a8a5bf59e285b9d58fdd87c7af8d395a6b50
Omar AlQasmi <omar.qasmi@innovateq.digital>
2023-06-15 14:09:47 +0400

    fix 2

--- Sources/OTextField/OTextField.swift
+++ Sources/OTextField/OTextField.swift
@@ -3,14 +3,14 @@ import UIKit
 public class OTextField: UIView {
 
     
-    @IBOutlet weak var vwContainer: UIView!
-    @IBOutlet weak var vwBorderLine: UIView!
-    @IBOutlet weak var txtFieldInput: UITextField!
-    @IBOutlet weak var imgPRE: UIImageView!
-    @IBOutlet weak var lblStatusMessage: UILabel!
-    @IBOutlet weak var imgPOST: UIImageView!
-    @IBOutlet weak var btnShowAndHidePassword: UIButton!
     
+    @IBOutlet weak var vwContentContainer: UIView!
+    @IBOutlet weak var vwBorderLineView: UIView!
+    @IBOutlet weak var lblErrorMessage: UILabel!
+    @IBOutlet weak var imgPreIcon: UIImageView!
+    @IBOutlet weak var txtInputField: UITextField!
+    @IBOutlet weak var imgPostIcon: UIImageView!
+    @IBOutlet weak var btnPasswordVisibility: UIButton!
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -23,87 +23,87 @@ public class OTextField: UIView {
     }
     public func commonInit() {
         Bundle.module.loadNibNamed("OTextField", owner: self, options: nil)
-        vwContainer.fixInViewOTextField(self)
+        vwContentContainer.fixInViewOTextField(self)
     }
 
     public func initText(preImage:UIImage?, placeHolder:String?, isPassword:Bool){
-        vwBorderLine.backgroundColor = .lightGray
-        txtFieldInput.delegate = self
-        imgPRE.isHidden = true
-        lblStatusMessage.isHidden = true
-        lblStatusMessage.textColor = .gray
-        txtFieldInput.placeholder = placeHolder
-        imgPRE.image = UIImage.init(named: "OTextField_envelope.fill", in: .module, compatibleWith: nil)!
-        imgPOST.image = UIImage.init(named: "OTextField_eye.slash", in: .module, compatibleWith: nil)!
+        vwBorderLineView.backgroundColor = .lightGray
+        txtInputField.delegate = self
+        imgPreIcon.isHidden = true
+        lblErrorMessage.isHidden = true
+        lblErrorMessage.textColor = .gray
+        txtInputField.placeholder = placeHolder
+        imgPreIcon.image = UIImage.init(named: "OTextField_envelope.fill", in: .module, compatibleWith: nil)!
+        imgPostIcon.image = UIImage.init(named: "OTextField_eye.slash", in: .module, compatibleWith: nil)!
 
         if preImage != nil {
-            imgPRE.isHidden = false
-            imgPRE.image = preImage
+            imgPreIcon.isHidden = false
+            imgPreIcon.image = preImage
 
         }else{
-            imgPRE.isHidden = true
+            imgPreIcon.isHidden = true
         }
         if placeHolder != nil {
-            txtFieldInput.placeholder = placeHolder
+            txtInputField.placeholder = placeHolder
         }else{
-            txtFieldInput.placeholder = ""
+            txtInputField.placeholder = ""
             
         }
         if isPassword {
-            btnShowAndHidePassword.isHidden = false
-            imgPOST.isHidden = false
-            txtFieldInput.isSecureTextEntry = true
+            btnPasswordVisibility.isHidden = false
+            imgPostIcon.isHidden = false
+            txtInputField.isSecureTextEntry = true
         }else{
-            btnShowAndHidePassword.isHidden = true
-            imgPOST.isHidden = true
-            txtFieldInput.isSecureTextEntry = false
+            btnPasswordVisibility.isHidden = true
+            imgPostIcon.isHidden = true
+            txtInputField.isSecureTextEntry = false
 
         }
 
     }
-    @IBAction func btnActionShowOrHidePassword(_ sender: Any) {
-        txtFieldInput.isSecureTextEntry = !txtFieldInput.isSecureTextEntry
-        if txtFieldInput.isSecureTextEntry {
-            imgPOST.image = UIImage.init(named: "OTextField_eye.slash", in: .module, compatibleWith: nil)!
+    @IBAction func btnActionPasswordVisibilty(_ sender: Any) {
+        txtInputField.isSecureTextEntry = !txtInputField.isSecureTextEntry
+        if txtInputField.isSecureTextEntry {
+            imgPostIcon.image = UIImage.init(named: "OTextField_eye.slash", in: .module, compatibleWith: nil)!
 
         }else{
-            imgPOST.image = UIImage.init(named: "OTextField_eye", in: .module, compatibleWith: nil)!
+            imgPostIcon.image = UIImage.init(named: "OTextField_eye", in: .module, compatibleWith: nil)!
         }
-
     }
+    
     public func getText() -> String {
-        return txtFieldInput.text ?? ""
+        return txtInputField.text ?? ""
     }
     public func failure(withMessage: String? = nil) {
-        vwBorderLine.backgroundColor = .red
+        vwBorderLineView.backgroundColor = .red
         if withMessage != nil {
-            lblStatusMessage.isHidden = false
-            lblStatusMessage.text = withMessage
-            lblStatusMessage.textColor = .red
+            lblErrorMessage.isHidden = false
+            lblErrorMessage.text = withMessage
+            lblErrorMessage.textColor = .red
         }else{
-            lblStatusMessage.isHidden = true
-            lblStatusMessage.textColor = .gray
+            lblErrorMessage.isHidden = true
+            lblErrorMessage.textColor = .gray
 
         }
 
     }
     public func success(withMessage: String? = nil){
-        vwBorderLine.backgroundColor = .green
+        vwBorderLineView.backgroundColor = .green
         if withMessage != nil {
-            lblStatusMessage.isHidden = false
-            lblStatusMessage.text = withMessage
-            lblStatusMessage.textColor = .green
+            lblErrorMessage.isHidden = false
+            lblErrorMessage.text = withMessage
+            lblErrorMessage.textColor = .green
         }else{
-            lblStatusMessage.isHidden = true
-            lblStatusMessage.textColor = .gray
+            lblErrorMessage.isHidden = true
+            lblErrorMessage.textColor = .gray
 
         }
 
     }
     public func clearMessage(){
-        vwBorderLine.backgroundColor = .lightGray
-        lblStatusMessage.isHidden = true
-        lblStatusMessage.textColor = .gray
+        vwBorderLineView.backgroundColor = .lightGray
+        lblErrorMessage.isHidden = true
+        lblErrorMessage.textColor = .gray
 
     }
 
